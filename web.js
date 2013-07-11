0f428691bc06d6b0ad1d1dff632294ef4d4a351d
Sun Tzu <suntzu@gmail.com>
2013-07-11 10:40:50 +0000

    Added index.html and updated code in web.js to pull from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  // response.send('Hello World2!');
+	var data = new Buffer(fs.readFileSync('index.html'));
+	response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
