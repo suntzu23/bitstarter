f0234cd0a4da827d76365cc44c6396ab2da95f50
Sun Tzu <suntzu@gmail.com>
2013-07-14 06:35:48 +0000

    takes website as an argument

--- grader.js
+++ grader.js
@@ -1,30 +1,42 @@
 #!/usr/bin/env node
+/*
+Automatically grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
+
+References:
+
+ + cheerio
+   - https://github.com/MatthewMueller/cheerio
+   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+   - http://maxogden.com/scraping-with-node.html
+
+ + commander.js
+   - https://github.com/visionmedia/commander.js
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+
+ + JSON
+   - http://en.wikipedia.org/wiki/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+*/
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var rest = require('restler');
+var rest = require("restler");
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
+    var error_msg = "";
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
-
-var website = function(url) {
-	rest.get(url).on('complete', function(result) {
-	if (result instanceof Error) {
-		console.err('Error: ' + result.message);
-	} else {
-		return result;
-	}
-});
-}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -51,20 +63,37 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var get_website = function(url) {
+    rest.get(url).on('complete', function(result) {
+        if(result instanceof Error) {
+            console.log("site %s not found.", url);
+            process.exit(1);
+        } else {
+            var htmlfile = "response.html";
+            fs.writeFileSync(htmlfile, result);
+            checkings(htmlfile);
+        } 
+    });
+};
+
+var checkings = function(file) {
+    var checkJson = checkHtmlFile(file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file [html_file]', 'Path to index.html', HTMLFILE_DEFAULT)
-	.option('-u, --url [url]', 'URL of website')
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'File Url')
         .parse(process.argv);
-	if (typeof program.url !== 'undefined' && program.url) {
-		var checkJson = checkHtmlFile(website(program.url), program.checks);
-	}
-	else {
-		var checkJson = checkHtmlFile(program.file, program.checks);
-	}
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if( program.url ) {
+        get_website(program.url);
+    } else {
+        checkings(program.file);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
